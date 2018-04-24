ee0baa51ffdae00b0d37738174835f063ca8b585
Maksymilian Knoski <maks.knoski@gmail.com>
2018-04-24 18:38:35 +0200

    restoring npm in build

--- Build.bat
+++ Build.bat
@@ -1,13 +1,30 @@
 @echo OFF
 
+cd LearnMore.Angular\
+
+IF NOT EXIST node_modules GOTO RESTORE_NPM
+
+:BUILD_ANGULAR
 ECHO Building Angular...
 ECHO.
-cd LearnMore.Angular\
 ng build | more
+
 cd ..
 
+GOTO BUILD_DOTNET
+
+:BUILD_DOTNET
 ECHO Building Dotnet...
 ECHO.
 cd LearnMore.Api\
 dotnet build | more
 cd ..
+
+EXIT
+
+:RESTORE_NPM
+ECHO Restoring NPM...
+ECHO.
+npm install | more
+GOTO BUILD_ANGULAR
+
